0b156fc65a7cc4f1499fbd6a7d62d6c6c186580f
Denny Check <dcheck@andrew.cmu.edu>
2019-12-06 20:43:45 -0500

    push

--- PUI7wirking/assets/js/script.js
+++ PUI7wirking/assets/js/script.js
@@ -54,6 +54,7 @@ $('#sorts').on( 'click', 'button', function() {
   $grid.isotope("reloadItems").isotope({ sortBy: sortByValue });
   console.log("SORT");
   console.log(sortByValue);
+  console.log(colorList);
 });
 
 // change is-checked class on buttons
@@ -65,30 +66,29 @@ $('.button-group').each( function( i, buttonGroup ) {
   });
 });
 
-const pickr7 = new Pickr({
-  el: '#color-picker-7',
-  useAsButton: true,
+var colorList = [ '000000', '993300', '333300', '003300', '003366', '000066', '333399', '333333', 
+'660000', 'FF6633', '666633', '336633', '336666', '0066FF', '666699', '666666', 'CC3333', 'FF9933', '99CC33', '669966', '66CCCC', '3366FF', '663366', '999999', 'CC66FF', 'FFCC33', 'FFFF66', '99FF66', '99CCCC', '66CCFF', '993366', 'CCCCCC', 'FF99CC', 'FFCC99', 'FFFF99', 'CCffCC', 'CCFFff', '99CCFF', 'CC99FF', 'FFFFFF' ];
+    var picker = $('#color-picker');
 
-  components: {
-    preview: true,
-    opacity: true,
-    hue: true,
-
-    interaction: {
-      hex: true,
-      rgba: true,
-      hsla: true,
-      hsva: true,
-      cmyk: true,
-      input: true,
-      clear: true,
-      save: true
+    for (var i = 0; i < colorList.length; i++ ) {
+      picker.append('<li class="color-item" data-hex="' + '#' + colorList[i] + '" style="background-color:' + '#' + colorList[i] + ';"></li>');
     }
-  },
 
-  onChange(hsva) {
-    $(document.body).css('background-color', hsva.toHEX().toString());
-  }
-});
+    $('body').click(function () {
+      picker.hide();
+    });
+
+    $('.call-picker').click(function(event) {
+      event.stopPropagation();
+      picker.show();
+      console.log("HEYYYYY");
+      picker.children('li').hover(function() {
+        var codeHex = $(this).data('hex');
+
+        $('.color-holder').css('background-color', codeHex);
+        $('body').css('background-color', codeHex);
+        $('#pickcolor').val(codeHex);
+      });
+    });
 
   
