ff542dd5e2c22c72197f59930b245e32281b7aab
Denny Check <dcheck@andrew.cmu.edu>
2019-12-06 23:59:23 -0500

    final2

--- PUI7wirking/assets/js/script.js
+++ PUI7wirking/assets/js/script.js
@@ -26,7 +26,7 @@ var $grid = $('.grid').isotope({
   }
 });
 
-// filter functions
+// filter functions, based on https://isotope.metafizzy.co/
 var filterFns = {
   // show if number is greater than 50
   numberGreaterThan50: function() {
@@ -65,6 +65,8 @@ $('.button-group').each( function( i, buttonGroup ) {
     $( this ).addClass('is-checked');
   });
 });
+
+//color picker setup based on https://codepen.io/voronianski/pen/zpahm
 
 var colorList = [ '000000', '993300', '333300', '003300', '003366', '000066', '333399', '333333', 
 '660000', 'FF6633', '666633', '336633', '336666', '0066FF', '666699', '666666', 'CC3333', 'FF9933', '99CC33', '669966', '66CCCC', '3366FF', '663366', '999999', 'CC66FF', 'FFCC33', 'FFFF66', '99FF66', '99CCCC', '66CCFF', '993366', 'CCCCCC', 'FF99CC', 'FFCC99', 'FFFF99', 'CCffCC', 'CCFFff', '99CCFF', 'CC99FF', 'FFFFFF' ];
