c20a2bfc0788aab5317eb7dfb6865d967bda40df
Denny Check <dcheck@andrew.cmu.edu>
2019-12-18 02:37:30 -0500

    push

---
+++ icon explorer/assets/js/script.js
@@ -0,0 +1,96 @@
+// external js: isotope.pkgd.js
+
+
+$('.basic').spectrum({
+    color: "#f00",
+    change: function(color) {
+        $("#basic-log").text("change called: " + color.toHexString());
+    }
+});
+
+// init Isotope
+var $grid = $('.grid').isotope({
+  itemSelector: '.element-item',
+  layoutMode: 'fitRows',
+  getSortData: {
+    name: '.name',
+    symbol: '.symbol',
+    color: '.color',
+    date: '.date',
+    number: '.number parseInt',
+    category: '[data-category]',
+    weight: function( itemElem ) {
+      var weight = $( itemElem ).find('.weight').text();
+      return parseFloat( weight.replace( /[\(\)]/g, '') );
+    }
+  }
+});
+
+// filter functions, based on https://isotope.metafizzy.co/
+var filterFns = {
+  // show if number is greater than 50
+  numberGreaterThan50: function() {
+    var number = $(this).find('.number').text();
+    return parseInt( number, 10 ) > 50;
+  },
+  // show if name ends with -ium
+  ium: function() {
+    var name = $(this).find('.name').text();
+    return name.match( /ium$/ );
+  }
+};
+
+// bind filter button click
+$('#filters').on( 'click', 'button', function() {
+  var filterValue = $( this ).attr('data-filter');
+  // use filterFn if matches value
+  filterValue = filterFns[ filterValue ] || filterValue;
+  $grid.isotope({ filter: filterValue });
+});
+
+// bind sort button click
+$('#sorts').on( 'click', 'button', function() {
+  var sortByValue = $(this).attr('data-sort-by');
+  $grid.isotope("reloadItems").isotope({ sortBy: sortByValue });
+  console.log("SORT");
+  console.log(sortByValue);
+  console.log(colorList);
+});
+
+// change is-checked class on buttons
+$('.button-group').each( function( i, buttonGroup ) {
+  var $buttonGroup = $( buttonGroup );
+  $buttonGroup.on( 'click', 'button', function() {
+    $buttonGroup.find('.is-checked').removeClass('is-checked');
+    $( this ).addClass('is-checked');
+  });
+});
+
+//color picker setup based on https://codepen.io/voronianski/pen/zpahm
+
+var colorList = [ '000000', '993300', '333300', '003300', '003366', '000066', '333399', '333333', 
+'660000', 'FF6633', '666633', '336633', '336666', '0066FF', '666699', '666666', 'CC3333', 'FF9933', '99CC33', '669966', '66CCCC', '3366FF', '663366', '999999', 'CC66FF', 'FFCC33', 'FFFF66', '99FF66', '99CCCC', '66CCFF', '993366', 'CCCCCC', 'FF99CC', 'FFCC99', 'FFFF99', 'CCffCC', 'CCFFff', '99CCFF', 'CC99FF', 'FFFFFF' ];
+    var picker = $('#color-picker');
+
+    for (var i = 0; i < colorList.length; i++ ) {
+      picker.append('<li class="color-item" data-hex="' + '#' + colorList[i] + '" style="background-color:' + '#' + colorList[i] + ';"></li>');
+    }
+
+    $('body').click(function () {
+      picker.hide();
+    });
+
+    $('.call-picker').click(function(event) {
+      event.stopPropagation();
+      picker.show();
+      console.log("HEYYYYY");
+      picker.children('li').hover(function() {
+        var codeHex = $(this).data('hex');
+
+        $('.color-holder').css('background-color', codeHex);
+        $('body').css('background-color', codeHex);
+        $('#pickcolor').val(codeHex);
+      });
+    });
+
+  
